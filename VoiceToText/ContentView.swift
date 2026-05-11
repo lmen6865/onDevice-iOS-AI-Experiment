15e8e893ac75d910a00c21e11ffb841c7945b4fc
lmen6865 <Lahiru.Menikdiwela@sysco.com>
2026-05-11 14:57:12 +0530

    Initial Commit

---
+++ VoiceToText/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  VoiceToText
+//
+//  Created by Menikdiwela, Lahiru 588 on 2026-05-11.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ VoiceToText/VoiceToTextApp.swift
@@ -0,0 +1,17 @@
+//
+//  VoiceToTextApp.swift
+//  VoiceToText
+//
+//  Created by Menikdiwela, Lahiru 588 on 2026-05-11.
+//
+
+import SwiftUI
+
+@main
+struct VoiceToTextApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
